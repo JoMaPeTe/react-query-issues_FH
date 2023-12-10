f19d471abaf2be4a80d25e8920a9431ea1a41a65
JoseMPeTe <jmperezterrero@gmail.com>
2023-12-10 19:38:49 +0100

    filtro time since

--- src/issues/hooks/useIssues.tsx
+++ src/issues/hooks/useIssues.tsx
@@ -7,17 +7,23 @@ interface Props {
 	state?: State;
 	labels: string[];
 }
-const getIssues = async (labels: string[], state?:State): Promise<Issue[]> => {
+const getIssues = async (labels: string[], state?: State): Promise<Issue[]> => {
 	await sleep(2);
 	const params = new URLSearchParams();
-	if(state)params.append('state', state)
-	const { data } = await githubAPI.get<Issue[]>("/issues", {params});
+	if (state) params.append("state", state);
+	if (labels.length > 0) {
+		const labelString = labels.join(",");
+		params.append("labels", labelString);
+	}
+	params.append("page", "1");
+	params.append("per_page", "5");
+	const { data } = await githubAPI.get<Issue[]>("/issues", { params });
 	return data;
 };
-export const useIssues = ({state, labels}:Props) => {
+export const useIssues = ({ state, labels }: Props) => {
 	const issuesQuery = useQuery({
-		queryKey: ["issues",{ state, labels}],
-		queryFn: getIssues,
+		queryKey: ["issues", { state, labels }],
+		queryFn: () => getIssues(labels, state),
 	});
 	return {
 		issuesQuery,
